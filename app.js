de1bc67098a01eca874228ce92bed06e7de3d113
EslamOsama1 <asamhaslam062@gmail.com>
2026-08-30 20:32:20 +0300

    cofigure cors

--- app.js
+++ app.js
@@ -30,7 +30,10 @@ app.use(express.json({ limit: '10kb' }))
 
 // Security
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
 
 // Rate Limiting
 const limiter = rateLimit({
